941ea52858acb8dec622e1dec029a60ba7e29233
BarukhOr <Ensisong01@gmail.com>
2014-11-18 22:12:41 -0500

    Incorporated Landing Page (From Susan)
    -------------------------------------------
    Changes:
    Moved Javascript code to a separate file (js/main.js)
    Moved Optimized the placement of the script tags on the landing page

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+	$(function() {
+		$('#vendorBlock').hover(function() {
+			$('#vendorBlockTitle').css('margin-top', '64px');
+			$('#vendorBlock .arrow').css('margin-top', '24px');
+			$('#vendorText').css('opacity','1');
+		}, function() {
+	    	// on mouseout, reset styling
+	    	$('#vendorBlockTitle').css('margin-top', '128px');
+			$('#vendorBlock .arrow').css('margin-top', '-64px');
+			$('#vendorText').css('opacity','0');
+		});
+	});
+	$(function() {
+		$('#clientBlock').hover(function() {
+			$('#clientBlockTitle').css('margin-top', '64px');
+			$('#clientBlock .arrow').css('margin-top', '24px');
+			$('#clientText').css('opacity','1');
+		}, function() {
+	    	// on mouseout, reset styling
+	    	$('#clientBlockTitle').css('margin-top', '128px');
+			$('#clientBlock .arrow').css('margin-top', '-64px');
+			$('#clientText').css('opacity','0');
+		});
+	});
