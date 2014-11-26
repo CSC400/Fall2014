97b2b64d620a415087eea1cfe1aee7c99ed7fb61
BarukhOr <Ensisong01@gmail.com>
2014-11-25 21:57:46 -0500

    Added javascript snippet provided by Data Analytics

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,29 @@
-	$(function() {
-		$('#vendorBlock').hover(function() {
-			$('#vendorBlockTitle').css('margin-top', '64px');
-			$('#vendorBlock .arrow').css('margin-top', '24px');
-			$('#vendorText').css('opacity','1');
-		}, function() {
-	    	// on mouseout, reset styling
-	    	$('#vendorBlockTitle').css('margin-top', '128px');
-			$('#vendorBlock .arrow').css('margin-top', '-64px');
-			$('#vendorText').css('opacity','0');
-		});
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-56026003-1']);
+_gaq.push(['_trackPageview']);
+
+$(function() {
+	$('#vendorBlock').hover(function() {
+		$('#vendorBlockTitle').css('margin-top', '64px');
+		$('#vendorBlock .arrow').css('margin-top', '24px');
+		$('#vendorText').css('opacity','1');
+	}, function() {
+    	// on mouseout, reset styling
+    	$('#vendorBlockTitle').css('margin-top', '128px');
+		$('#vendorBlock .arrow').css('margin-top', '-64px');
+		$('#vendorText').css('opacity','0');
 	});
-	$(function() {
-		$('#clientBlock').hover(function() {
-			$('#clientBlockTitle').css('margin-top', '64px');
-			$('#clientBlock .arrow').css('margin-top', '24px');
-			$('#clientText').css('opacity','1');
-		}, function() {
-	    	// on mouseout, reset styling
-	    	$('#clientBlockTitle').css('margin-top', '128px');
-			$('#clientBlock .arrow').css('margin-top', '-64px');
-			$('#clientText').css('opacity','0');
-		});
+});
+
+$(function() {
+	$('#clientBlock').hover(function() {
+		$('#clientBlockTitle').css('margin-top', '64px');
+		$('#clientBlock .arrow').css('margin-top', '24px');
+		$('#clientText').css('opacity','1');
+	}, function() {
+    	// on mouseout, reset styling
+    	$('#clientBlockTitle').css('margin-top', '128px');
+		$('#clientBlock .arrow').css('margin-top', '-64px');
+		$('#clientText').css('opacity','0');
 	});
+});
