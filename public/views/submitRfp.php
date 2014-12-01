a5b9f44d274a6e36448bcd6173e7bc74b3a507da
Baruch Or <Baruch Or>
2014-12-01 13:33:16 -0500

    Fixed problem with inserting rfps to database and also removed unused files

--- public/views/submitRfp.php
+++ public/views/submitRfp.php
@@ -1,37 +1,44 @@
 <?php
 	include "../classes/db_conn.php";
 	
-	#function submitRFP(){
-		$conn = dbconn();
-		$collection = new MongoCollection($conn, 'rfps');
+	$conn = dbconn();
+	$collection = new MongoCollection($conn, 'rfps');
+
+	$contact = $_POST['firstName']." ".$_POST['lastName'];
+	$email = $_POST['emailAddress'];
+	$phone = $_POST['phoneNumber'];
+	$address = $_POST['streetAddress'];
+	$city = $_POST['city'];
+	$rooms = $_POST['numberClassrooms'];
+	$seats = $_POST['numberSeats'];
+	$height = $_POST['height'];
+	$width = $_POST['width'];
+	$length = $_POST['length'];
+	$style = $_POST['classroomStyle'];
+	$podium = $_POST['instructorPodium'];
+	$projection = $_POST['projectionSystem'];
+	$media = $_POST['mediaSystem'];
+	$audio = $_POST['highQualityAudio'];
+	$maxbudget = $_POST['maxBudget'];
+	$compdate = $_POST['compDate'];
+	$summary = $_POST['summary'];
 	
-		$contact = $_POST['firstName']+ " " + $_POST['lastName'];
-		$email = $_POST['emailAddress'];
-		$phone = $_POST['phoneNumber'];
-		$address = $_POST['streetAddress'];
-		$city = $_POST['city'];
-		$rooms = $_POST['numberClassrooms'];
-		$seats = $_POST['numberSeats'];
-		$height = $_POST['height'];
-		$width = $_POST['width'];
-		$length = $_POST['length'];
-		$style = $_POST['classroomStyle'];
-		$podium = $_POST['instructorPodium'];
-		$projection = $_POST['projectionSystem'];
-		$media = $_POST['mediaSystem'];
-		$audio = $_POST['highQualityAudio'];
-		$maxbudget = $_POST['maxBudget'];
-		$compdate = $_POST['compDate'];
-		$summary = $_POST['summary'];
-		
-		
-		try{
-			$collection->insert (array('Contact'=>$contact, 'RFP#'=>25552, 'Purpose'=>$style, 'Projection'=>$projection,
-										'Number of Rooms'=>$rooms, 'Stadium'=>$podium, 'Budget in $'=>$maxbudget, 'Completion Date'=>$compdate));
-		} 
-		catch(MongoException $e){
-			error_log($e."\n", 3, "../log-errors.log");
-		}
-	#}
+	// var_dump($contact);
+	
+	$infoArray = array('Contact'=>$contact, 'rfpnum'=>25552, 'purpose'=>$style, 'projection'=>$projection,
+									'classnum'=>$rooms, 'stadium'=>$podium, 'budget'=>$maxbudget, 'compdate'=>$compdate);
+	var_dump($infoArray);
+
+	echo "\n This is the value stored in in style: "; var_dump($style);
+	echo "\n This is the value stored in audio: ";
+	var_dump($audio);
+
+	try{
+		$collection->insert ($infoArray);
+		header("Location: clientLanding.html");
+	} 
+	catch(MongoException $e){
+		error_log($e."\n", 3, "../log-errors.log");
+	}
 	
 ?>
